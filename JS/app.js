5800a566f0bdcf78050ea26f0d7ab11800f3d933
Ashish Shrivastava <smartashishg442@gmail.com>
2025-01-12 21:25:46 +0530

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,49 @@
+const API_KEY = "8c3ff98b73c4ee6e1cd07024e57d4141";
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const dayImages = [
+    'dayImage/background1.webp', 
+    'dayImage/background2.webp', 
+    'dayImage/background3.webp', 
+    'dayImage/background4.webp', 
+    'dayImage/background5.webp', 
+    'dayImage/background6.webp', 
+    'dayImage/background7.webp', 
+    'dayImage/background8.webp',
+];
+
+const nightImages = [
+    'nightImage/background9.webp', 
+    'nightImage/background10.webp', 
+    'nightImage/background11.webp', 
+    'nightImage/background12.webp', 
+    'nightImage/background13.webp', 
+    'nightImage/background14.webp', 
+    'nightImage/background15.webp', 
+    'nightImage/background16.webp',
+];
+
+
+
